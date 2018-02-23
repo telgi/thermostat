610adafa37c1bdf7720c1d1ede69a6e52b9a9f11
Terry Allitt <tallitt88@gmail.com>
2018-02-23 16:17:09 +0000

    add some styling to make the interface more user friendly

--- public/js/interface.js
+++ public/js/interface.js
@@ -1,18 +1,35 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
 
-  $("#current-temp").text(thermostat.temp);
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=a3d9eb01d4de82b9b8d0849ef604dbed&';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data) {
+      $('#city-temp').text(data.main.temp);
+    })
+  }
+
+  displayWeather('London');
+
+  $('#select-city').submit(function(event) {
+    event.preventDefault();
+    var city = $('#current-city').val();
+    displayWeather(city);
+  })
+
+  $("#home-temp").text(thermostat.temp);
 
   $("#temp-up").click(function() {
     thermostat.up();
-    $("#current-temp").text(thermostat.temp);
+    $("#home-temp").text(thermostat.temp);
     thermostat.checkEnergyUsage();
     $("#energy-usage").text(thermostat.energyUsage);
   });
 
   $("#temp-down").click(function() {
     thermostat.down();
-    $("#current-temp").text(thermostat.temp);
+    $("#home-temp").text(thermostat.temp);
     thermostat.checkEnergyUsage();
     $("#energy-usage").text(thermostat.energyUsage);
   });
